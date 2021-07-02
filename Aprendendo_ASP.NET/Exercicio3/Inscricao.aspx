a8040dfcf5800234b840184b5f7ec7c191ea0492
Diógenes Góis <diogeneslagogois@hotmail.com>
2021-07-02 20:11:44 +0100

    exercicio3

---
+++ Aprendendo_ASP.NET/Exercicio3/Inscricao.aspx
@@ -0,0 +1,47 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Inscricao.aspx.cs" Inherits="Exercicio3.Inscricao" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="height: 397px">
+            Nome:&nbsp;
+            <asp:TextBox ID="tb_nome" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            Curso:
+            <asp:DropDownList ID="ddl_curso" runat="server" AutoPostBack="True">
+                <asp:ListItem>-------------</asp:ListItem>
+                <asp:ListItem>Medicina</asp:ListItem>
+                <asp:ListItem>Direito</asp:ListItem>
+                <asp:ListItem>Mecatrónica</asp:ListItem>
+                <asp:ListItem>Barbeiro</asp:ListItem>
+            </asp:DropDownList>
+&nbsp;<br />
+            <br />
+            Email:
+            <asp:TextBox ID="tb_email" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            Telemóvel:
+            <asp:TextBox ID="tb_telemovel" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            Género:
+            <asp:RadioButtonList ID="rbl_genero" runat="server">
+                <asp:ListItem>Masculino</asp:ListItem>
+                <asp:ListItem>Feminino</asp:ListItem>
+            </asp:RadioButtonList>
+            <br />
+            <br />
+            <asp:Button ID="btn_word" runat="server" OnClick="btn_word_Click" Text="Word" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="btn_excel" runat="server" OnClick="btn_excel_Click" Text="Excel" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Aprendendo_ASP.NET/Exercicio3/Inscricao.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+using System.Text;
+
+namespace Exercicio3 {
+    public partial class Inscricao : System.Web.UI.Page {
+        protected void Page_Load(object sender, EventArgs e) {
+
+        }
+
+        protected void btn_word_Click(object sender, EventArgs e) {
+            Response.Clear();
+            Response.AddHeader("Content-Disposition", "attachment; filename = ficheiro.doc");
+            Response.ContentType = "application/msword";
+
+            StringWriter sw = new StringWriter();
+            HtmlTextWriter htw = new HtmlTextWriter(sw);
+
+            Response.ContentEncoding = Encoding.Unicode;
+            Response.BinaryWrite(Encoding.Unicode.GetPreamble());
+
+            htw.Write($"<table><b>Nome:</b>{tb_nome.Text}<br><b>Curso:</b>{ddl_curso.SelectedItem}<br><b>Email:</b>{tb_email.Text}<br><b>Telemóvel:</b>{tb_telemovel.Text}<br><b>Género:</b>{rbl_genero.SelectedItem}<br></table>");
+
+            Response.Write(sw);
+            Response.End();
+        }
+
+        protected void btn_excel_Click(object sender, EventArgs e) {
+            Response.Clear();
+            Response.AddHeader("Content-Disposition", "attachment; filename = ficheiro.xls");
+            Response.ContentType = "application/vnd.msexcel";
+
+            StringWriter sw = new StringWriter();
+            HtmlTextWriter htw = new HtmlTextWriter(sw);
+
+            Response.ContentEncoding = Encoding.Unicode;
+            Response.BinaryWrite(Encoding.Unicode.GetPreamble());
+
+            htw.Write($"<table><tr><td><b>Nome</b></td><td>{tb_nome.Text}</td></tr>" +
+                $"<tr><td><b>Curso</b></td><td>{ddl_curso.SelectedItem}</td></tr>" +
+                $"<tr><td><b>Email</b></td><td>{tb_email.Text}</td></tr>" +
+                $"<tr><td><b>Telemóvel</b></td><td align = 'left'>{tb_telemovel.Text}</td></tr>" +
+                $"<tr><td><b>Género</b></td><td>{rbl_genero.SelectedItem}</td></tr></table>");
+
+            Response.Write(sw);
+            Response.End();
+        }
+    }
+}
